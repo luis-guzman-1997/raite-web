98fe3c5954a95ddd834674224ca5bdba8d2e36f0
luis-guzman-1997 <luisespinoza.l.e108@gmail.com>
2024-11-17 10:41:08 -0600

    Ordenado de componentes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { SlideComponent } from '../../shared/slide/slide.component';
+import { TitleSubtitleComponent } from '../../shared/title-subtitle/title-subtitle.component';
+import { LoginGoogleComponent } from '../../components/login-google/login-google.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    SlideComponent,
+    TitleSubtitleComponent,
+    LoginGoogleComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/title-subtitle/title-subtitle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-subtitle',
+  standalone: true,
+  imports: [],
+  templateUrl: './title-subtitle.component.html',
+  styleUrl: './title-subtitle.component.scss'
+})
+export class TitleSubtitleComponent {
+
+}
